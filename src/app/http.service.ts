318cad30dbfad5778065573651039359df761319
Sergej Grilborzer <sergej1995@gmail.com>
2019-04-09 14:21:29 +0200

    Fully implement http Service

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -3,15 +3,37 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
 
 import { environment } from "src/environments/environment";
+import { Bike } from "./entities";
 
 @Injectable({
   providedIn: "root"
 })
 export class HttpService {
 
+  httpOptions = { headers: new HttpHeaders({ "Content-Type": "application/json" }) };
+
+  bikeStatusUrl = "/bike-status";
+  ownerAddBikeUrl = "/owner/add-bike";
+  ownerUpdateBikeUrl = "/owner/update-bike";
+  ownerGetBikeUrl = "/owner/get-bike";
+
   constructor(private http: HttpClient) { }
 
-  getBikeStatus(id: number) {
+  getBikeStatus(id: number): Observable<Bike> {
+    return this.http.get<Bike>(environment.serverUrl + this.bikeStatusUrl + `/{id}`, this.httpOptions);
+  }
 
+  ownerAddBike(bike: Bike): Observable<number> {
+    const bikeJson = JSON.stringify({bike});
+    return this.http.post<number>(environment.serverUrl + this.ownerAddBikeUrl, bikeJson, this.httpOptions);
+  }
+
+  ownerUpdateBike(bike: Bike): Observable<boolean> {
+    const bikeJson = JSON.stringify({bike});
+    return this.http.post<boolean>(environment.serverUrl + this.ownerUpdateBikeUrl, bikeJson, this.httpOptions);
+  }
+
+  ownerGetBike(id: number): Observable<Bike> {
+    return this.http.get<Bike>(environment.serverUrl + this.ownerGetBikeUrl + `/{id}`, this.httpOptions);
   }
 }
